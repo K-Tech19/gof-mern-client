35d711398621dc9dd7f15e5ba50fbc8fd79272ab
tkadima <kadima.t@husky.neu.edu>
2021-02-23 19:13:17 -0500

    did profile page added error files

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Profile.css'
 
 const Profile = (props) => {
     console.log(props);
     const userData = props.user ? 
-    (<div>
-        <h1>Profile</h1>
-        <p><strong>Email:</strong> {props.user.email}</p> 
-        <p><strong>ID:</strong> {props.user.id}</p> 
+    (<div className='profile'>
+        <p className='email'><strong>Email:</strong> {props.user.email}</p> 
+        <p className='ID'><strong>ID:</strong> {props.user.id}</p> 
     </div>) : <h4>Loading...</h4>
 
     const errorDiv = () => {
         return (
+           
             <div className="text-center pt-4">
                 <h3>Please <Link to="/login">login</Link> to view this page</h3>
             </div>
+           
         );
     };
-    
+   
     return (
         <div>
             { props.user ? userData : errorDiv() }
