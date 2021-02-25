2240ee47e549920696f0168fe1f885a11c38ec4b
tkadima <kadima.t@husky.neu.edu>
2021-02-25 12:13:17 -0500

    swapped blog page and profile page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,21 +7,23 @@ export default function Blog(){
         <div className='blog'>
             <img />
             <div className='col-md-7 offset-md-3'>
-                <div className='card card-body'>
-                <h2 id='title' className="py-2">Blog Post</h2>
-                    <form>
-                <div className="form-group">
-                    <label>Blog Title</label>
-                        <input type="text" className='form-control' />
+<div className="card card-body">
+    
+    <form>
+        <div id='blogtitle' className="form-group">
+            <label id='blogtitle'>Title</label>
+             <input type='text' className='form-control' />
+                <label id='blogcontent'>Content</label>
+                    <input type='text' className='form-control' />
                     </div>
-                    <div className='form-group'>
-                <label>Blog Post</label>
-                <textarea className='form-control' />
-            </div>
-            <button type="submit" className="btn btn-primary float-right">Submit</button>
-        </form>
-        </div> 
-        </div>  
+                <div id='blogcomments'className='form-group'>
+            <label>Comment</label>
+        <textarea className='form-control' />
+    <button type="submit" className="btn btn-primary float-right">Submit</button>
+</div>
+</form>
+</div>
+</div> 
         </div>
     )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,10 +6,28 @@ const Profile = (props) => {
     console.log(props);
     const userData = props.user ? 
     (<div className='profile'>
-        <p className='email'><strong>Email:</strong> {props.user.email}</p> 
+     <div >
+            
+            <div className='col-md-7 offset-md-3'>
+                <div className='card card-body' id='profilebody'>
+                <h2 id='profiletitle' className="py-2">Blog Post</h2>
+                    <form>
+                <div className="form-group">
+                    <label id='blogtitle'>Blog Title</label>
+                        <input type="text" className='form-control' />
+                    </div>
+                    <div className='form-group'>
+                <label id='bloginfo'>Blog Info</label>
+                <textarea className='form-control' />
+            </div>
+        </form>
+        </div> 
+        </div>  
+        </div>
+        {/* <p className='email'><strong></strong> {props.user.email}</p>  */}
         {/* <p className='ID'><strong>ID:</strong> {props.user.id}</p>  */}
     </div>) : <h4>Loading...</h4>
-
+                    
     const errorDiv = () => {
         return (
             <form>
